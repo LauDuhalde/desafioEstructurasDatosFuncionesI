27ac173ba5ff023954a6f57cfc9095a1e7f26b8f
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-02-29 13:38:40 -0300

    Validación de argumentos

--- word_count.py
+++ word_count.py
@@ -1,21 +1,25 @@
 import sys
-archivo = sys.argv[1]
+#Validación para evitar error al no ejecutar con argumento
+if(len(sys.argv)==2):
+    archivo = sys.argv[1]
+    
+    #Lee el archivo y guarda el contenido en la variable texto
+    with open(archivo, 'r') as file:
+        texto = file.read()
 
-#Lee el archivo y guarda el contenido en la variable texto
-with open(archivo, 'r') as file:
-    texto = file.read()
+    #Transforma el texto en una lista de palabras al separar
+    palabras = texto.split(' ')
 
-#Transforma el texto en una lista de palabras al separar
-palabras = texto.split(' ')
+    #Conjuntos para almacenar elementos únicos
+    caracteres_diferentes = set()
+    palabras_unicas = set(palabras) #palabras es una lista por lo que se puede transformar directamente en conjunto
 
-#Conjuntos para almacenar elementos únicos
-caracteres_diferentes = set()
-palabras_unicas = set(palabras) #palabras es una lista por lo que se puede transformar directamente en conjunto
-
-for palabra in palabras_unicas:
-    for caracter in palabra:
-        caracteres_diferentes.add(caracter)
+    for palabra in palabras_unicas:
+        for caracter in palabra:
+            caracteres_diferentes.add(caracter)
         
-#Ya que hicimos split por espacio, este no se contará, por lo que se debe sumar
-print(f"El número de caracteres distintos es: {len(caracteres_diferentes)+1}")
-print(f"El número de palabras distintas es: {len(palabras_unicas)}")
+    #Ya que hicimos split por espacio, este no se contará, por lo que se debe sumar
+    print(f"El número de caracteres distintos es: {len(caracteres_diferentes)+1}")
+    print(f"El número de palabras distintas es: {len(palabras_unicas)}")
+else:
+    print("Por favor ejecutar con el nombre de archivo a evaluar como argumento")
